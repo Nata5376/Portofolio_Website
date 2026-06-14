fa667871287e66e97d70afb126a35e68cc089f39
Nata5376 <gara122456@gmail.com>
2026-06-14 12:27:22 +0700

    setting

--- portofolio_project/settings.py
+++ portofolio_project/settings.py
@@ -108,9 +108,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'portfolio' / 'static',
+    BASE_DIR / 'static',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
